7c6b22c05dd9c71a41f377da7df653be45b95578
DanWilk <dcwilkinson@wilktech.com>
2022-01-11 15:40:27 -0500

    created User and Thoughts models

---
+++ models/Thoughts.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionText: {
+            type: String,
+            requires: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        }
+
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thoughts = model('Thoughts', ThoughtSchema);
+
+module.exports = Thoughts;
